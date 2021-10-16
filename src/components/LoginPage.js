930e1237612465a22c198da02944a4125e8178f8
luimu64 <leimuraittinen@gmail.com>
2021-10-17 00:25:13 +0300

    login form for login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const LoginForm = () => {
-    return ("nothing")
+    return (
+        <form class="pure-form pure-form-stacked" method="post" >
+            <fieldset>
+                <label for="username">Username</label>
+                <input type="text" id="username" placeholder="username" />
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Password" />
+                <button type="submit" class="pure-button pure-button-primary">Sign in</button>
+            </fieldset>
+        </form>
+    )
 }
 
 export default LoginForm;
